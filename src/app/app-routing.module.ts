bb4013cac64c2786687a280116907b0fb4c594a4
jimwellf <jimwellfulgencio@gmail.com>
2022-10-15 01:23:43 +0200

    fix: starting category component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
@@ -19,8 +20,8 @@ const routes: Routes = [
   {path:"catalog", component: CatalogComponent},
   {path:"login", component: LoginComponent},
   {path:"signup", component: SignupComponent},
+  {path:":category", component: ProductsComponent},
   {path:":category/:slug", component: ProductComponent},
-
   {path:"page-not-found", component: PagenotfoundComponent},
   {path:"**", redirectTo: "page-not-found"},
 ];
